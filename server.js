6cc9e82ddc489539a261c405aa2d2334ed35843c
Matt Oliver <mattoliver.mattoliver@gmail.com>
2017-03-07 22:53:45 -0600

    adding functionality

--- server.js
+++ server.js
@@ -4,11 +4,33 @@ var request = require('request');
 var cheerio = require('cheerio');
 var app     = express();
 
+//All the web scraping magic will happen here
 app.get('/scrape', function(req, res){
+// The URL we will scrape from - in our example Anchorman 2.
 
-  //All the web scraping magic will happen here
+    url = 'http://www.imdb.com/title/tt1229340/';
 
+    // The structure of our request call
+    // The first parameter is our URL
+    // The callback function takes 3 parameters, an error, response status code and the html
+
+    request(url, function(error, response, html){
+
+        // First we'll check to make sure no errors occurred when making the request
+
+        if(!error){
+            // Next, we'll utilize the cheerio library on the returned html which will essentially give us jQuery functionality
+
+            var $ = cheerio.load(html);
+
+            // Finally, we'll define the variables we're going to capture
+
+            var title, release, rating;
+            var json = { title : "", release : "", rating : ""};
+        }
+    })
 })
+
 
 app.listen('8081')
 
