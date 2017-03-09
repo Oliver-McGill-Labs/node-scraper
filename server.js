58293bfb5979100dced551c8096f25a6c7605eac
Matt Oliver <mattoliver.mattoliver@gmail.com>
2017-03-09 16:49:53 -0600

    updating server file

--- server.js
+++ server.js
@@ -19,16 +19,17 @@ app.get('/scrape', function(req, res){
         // First we'll check to make sure no errors occurred when making the request
 
         if(!error){
-            // Next, we'll utilize the cheerio library on the returned html which will essentially give us jQuery functionality
+            // Utilize the cheerio library on the returned html which will 
+            // essentially give us jQuery functionality
 
             var $ = cheerio.load(html);
 
-            // Finally, we'll define the variables we're going to capture
+            // FDefine the variables we're going to capture
 
             var title, release, rating;
             var json = { title : "", release : "", rating : ""};
 
-            // We'll use the unique header class as a starting point.
+            // Start with unique header class
 
             $('.header').filter(function(){
 
@@ -36,12 +37,14 @@ app.get('/scrape', function(req, res){
 
                 var data = $(this);
 
-           // In examining the DOM we notice that the title rests within the first child element of the header tag. 
-           // Utilizing jQuery we can easily navigate and get the text by writing the following code:
+           // In examining the DOM we notice that the title rests within the first child element 
+           // of the header tag. 
+
+           // Navigate and get the text:
 
                 title = data.children().first().text();
 
-           // Once we have our title, we'll store it to the our json object.
+           // Store title to the our json object.
 
                 json.title = title;
             })
