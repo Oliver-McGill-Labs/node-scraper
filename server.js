7cc11643d2167d58b42704054b289a10ccc22def
Matt Oliver <mattoliver.mattoliver@gmail.com>
2017-03-12 22:05:19 -0500

    writing to fs

--- server.js
+++ server.js
@@ -44,7 +44,8 @@ app.get('/scrape', function(req, res){
 
                 title = data.children().first().text();
 
-                // We will repeat the same process as above.  This time we notice that the release is located within the last element.
+                // We will repeat the same process as above.  
+                // This time we notice that the release is located within the last element.
                 // Writing this code will move us to the exact location of the release year.
 
                 release = data.children().last().children().text();
@@ -52,8 +53,42 @@ app.get('/scrape', function(req, res){
            // Store title and extracted data to the our json object.
 
                 json.title = title;
+                json.release = release;
+            })
+                 // Since the rating is in a different section of the DOM, we'll have to write a new 
+                 // Query filter to extract this information.
+
+            $('.star-box-giga-star').filter(function(){
+                var data = $(this);
+
+                // The .star-box-giga-star class was exactly where we wanted it to be.
+                // To get the rating, we can simply just get the .text(), no need to traverse the 
+                // DOM any further
+
+                rating = data.text();
+
+                json.rating = rating;
             })
         }
+
+        // To write to the system we will use the built in 'fs' library.
+		// In this example we will pass 3 parameters to the writeFile function
+		// Parameter 1 :  output.json - this is what the created filename will be called
+
+		// Parameter 2 :  JSON.stringify(json, null, 4) - the data to write, here we do an extra 
+		//				  step by calling JSON.stringify to make our JSON easier to read
+		
+		// Parameter 3 :  callback function - a callback function to let us know the status of our function
+
+		fs.writeFile('output.json', JSON.stringify(json, null, 4), function(err){
+
+		    console.log('File successfully written! - Check your project directory for the output.json file');
+
+		})
+
+		// Finally, we'll just send out a message to the browser reminding you that this app does not have a UI.
+		res.send('Check your console!')
+
     })
 })
 
