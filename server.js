23c9060022a375c0af0e4a43f04113cbc4a64ea5
Matt Oliver <mattoliver.mattoliver@gmail.com>
2017-03-20 16:36:36 -0500

    starting to scrape the TLO

--- server.js
+++ server.js
@@ -4,33 +4,43 @@ var request = require('request');
 var cheerio = require('cheerio');
 var app     = express();
 
+//All the web scraping magic will happen here
 app.get('/scrape', function(req, res){
-  // Let's scrape Anchorman 2
-  url = 'http://www.imdb.com/title/tt1229340/';
+  // Let's scrape the TLO and get the status of SB6
+  url = 'http://www.capitol.state.tx.us/BillLookup/BillStages.aspx?LegSess=85R&Bill=SB6';
 
   request(url, function(error, response, html){
     if(!error){
       var $ = cheerio.load(html);
 
-      var title, release, rating;
-      var json = { title : "", release : "", rating : ""};
+      var title, author, status;
+      var json = { title : "", author : "", status : ""};
 
-      $('.title_wrapper').filter(function(){
+      $('#usrBillInfoTabs_lblBill').filter(function(){
         var data = $(this);
-        title = data.children().first().text().trim();
-        release = data.children().last().children().last().text().trim();
-
+        title = data.text().trim();
         json.title = title;
-        json.release = release;
+        console.log('The bill\'s title was ' + title);
+      
       })
 
+      $('#usrBillInfoTabs_lblItem2Data').filter(function(){
+        var data = $(this);
+        author = data.text().trim();
+        json.author = author;
+        console.log('The bill\'s author was ' + author);
+      
+      })
+
+      $('.stageText').filter(function(){
+        var data = $(this);
+        
+        status = data.text().trim(); 
       
 
-      $('.ratingValue').filter(function(){
-        var data = $(this);
-        rating = data.text().trim();
-
-        json.rating = rating;
+        console.log('The bill\'s most recent status was ' + status);        
+        json.status = status;
+        
       })
     }
 
